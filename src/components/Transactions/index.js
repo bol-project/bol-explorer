68068afb3eaf6a09cc93e4abd836fcb6f3288406
Charalampos Chomenidis <hampos@me.com>
2024-01-03 12:03:01 +0200

    Add search input to transactions page

--- src/components/Transactions/index.js
+++ src/components/Transactions/index.js
@@ -1,55 +1,47 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
 
-import './style.css';
-import TransactionListElement from "../Home/TransactionListElement";
-import Row from "reactstrap/lib/Row";
-import Col from "reactstrap/lib/Col";
+import { FormGroup, Form, Input, InputGroup, Button } from "reactstrap";
+
+import "./style.css";
 
 class Transactions extends Component {
+  constructor() {
+    super();
+    this.searchValueChanged = this.searchValueChanged.bind(this);
+    this.navigateToTransaction = this.navigateToTransaction.bind(this);
+    this.state = { searchValue: "" };
+  }
 
-    constructor() {
-        super();
+  searchValueChanged(event) {
+    this.setState({ searchValue: event.target.value });
+  }
 
-        this.state = {
-        };
-    }
+  navigateToTransaction(event) {
+    event.preventDefault();
+    this.props.history.push("/transaction/" + this.state.searchValue);
+  }
 
-    componentWillMount() {          //the first true life cycle method: called one time, which is before the initial render
-        fetch('http://localhost:5000/api/transactions?Page=' + this.props.match.params.page + '&PageSize=20')
-            .then(res => res.json())
-            .then((data) => {
-                this.setState({ transactionActivityList: data.items.map(item => <TransactionListElement key={item.id} item={item}/>)})
-            })
-            .catch(console.log)
-    }
-
-    render() {
-
-        return(
-            <div className="view-page">
-                <div className="table-list">
-                    <h1>All transactions</h1>
-
-                    <div className="table-header btn btn-twitter ">
-                        <Row>
-                            <Col sm> <span>Transaction Type</span></Col>
-                            <Col sm><span>Transaction ID</span></Col>
-                            <Col sm><span>Timestamp</span></Col>
-                        </Row>
-                    </div>
-                    {this.state.transactionActivityList}
-                    <br/>
-                    <br/>
-                    <Link className={( (parseInt(this.props.match.params.page) > 1) ? '' : 'invisible')}
-                          to={`/transactions/${ parseInt(this.props.match.params.page) - 1}`} onClick={this.forceUpdate}>Previous</Link>
-                    <span> </span>
-                    <Link to={`/transactions/${ parseInt(this.props.match.params.page) + 1}`} onClick={this.forceUpdate}>Next</Link>
-                </div>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="view-page">
+        <Form className="form-inline" onSubmit={this.navigateToTransaction}>
+          <FormGroup style={{ width: '100%' }} className="no-border">
+            <InputGroup>
+              <Input
+                className={"search-field"}
+                style={{ width: '100%' }}
+                type="text"
+                placeholder="Enter Transaction Hash"
+                value={this.state.searchValue}
+                onChange={this.searchValueChanged}
+              />
+            </InputGroup>
+            <Button type="submit">Search</Button>
+          </FormGroup>
+        </Form>
+      </div>
+    );
+  }
 }
 
 export default Transactions;
