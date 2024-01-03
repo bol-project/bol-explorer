887938b16c855fa1eaace5366fc256752126a488
Charalampos Chomenidis <hampos@me.com>
2024-01-03 23:22:08 +0200

    Fix react initializationz

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 
-import './index.css';
+import "./index.css";
 import "./assets/css/nucleo-icons.css";
 import "./assets/scss/blk-design-system-react.scss?v=1.0.0";
 import "./assets/demo/demo.css";
 
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-import bolClientInstance from './client/bolClient';
-import BolContext from './bolContext';
+import bolClientInstance from "./client/bolClient";
+import BolContext from "./bolContext";
 
-global.Buffer = global.Buffer || require('buffer').Buffer;
+import { createRoot } from "react-dom/client";
+
+global.Buffer = global.Buffer || require("buffer").Buffer;
 
 await bolClientInstance.initialize();
 
-ReactDOM.render(
-    <BolContext.Provider value={bolClientInstance}>
-        <App />
-    </BolContext.Provider>, 
-    document.getElementById('root'));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <BolContext.Provider value={bolClientInstance}>
+    <App />
+  </BolContext.Provider>
+);
 
 serviceWorker.unregister();
